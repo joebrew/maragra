3b33961619b1094dfc7df0bb73e5a08358604f41
joebrew <joebrew@gmail.com>
2017-08-28 17:24:40 +0200

    done with preliminary analysis skeleton

--- R/run_all_analyses.R
+++ R/run_all_analyses.R
@@ -10,7 +10,7 @@
 #' @export
 
 run_all_analyses <- function(date = NULL,
-                               output_dir = NULL){
+                             output_dir = NULL){
   
   # If no output directory, make current wd
   if(is.null(output_dir)){
@@ -24,8 +24,14 @@ run_all_analyses <- function(date = NULL,
   
   # Clinical malaria incidence report
   message(paste0('Generating the clinical malaria incidence report in ',
-          output_dir))
+                 output_dir))
   generate_maragra_clinical_malaria_incidence(date = date,
                                               output_dir = output_dir)
+  
+  # Clinical preliminary analysis report
+  message(paste0('Generating the preliminary analysis report in ',
+                 output_dir))
+  generate_preliminary_analysis(date = date,
+                                output_dir = output_dir)
   
 }
